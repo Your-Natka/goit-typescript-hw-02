5fb6db880661181ba9273602e2c5399c31e95887
Your-Natka <140067844+Your-Natka@users.noreply.github.com>
2024-03-13 16:17:57 +0100

    commit_type2

--- src/baseTypes/1.ts
+++ src/baseTypes/1.ts
@@ -1,10 +1,13 @@
 // Перетворіть цей код на TypeScript, вказавши відповідні типи для всіх змінних.
-
-let age = 50;
-let name = 'Max';
-let toggle = true;
-let empty = null;
-let notInitialize;
-let callback = (a) => { return 100 + a };
+function foo(num: number, str: string, bool: boolean, empty: null) {
+  let age = 50;
+  let name = "Max";
+  let toggle = true;
+  let empty = null;
+  let notInitialize;
+  let callback = (a) => {
+    return 100 + a;
+  };
+}
 
 export {};
